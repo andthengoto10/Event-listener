947993f394747197023f917c57c9004eba6742fd
andthengoto10 <andthengoto10@gmail.com>
2019-10-23 12:52:05 +0200

    second commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let test = document.getElementById("test");
+  
+for(i=0; i<16; i++){
+  test.innerHTML += '<div class="item"></div>'
+}
+
+// This handler will be executed every time the cursor
+// is moved over a different div item
+test.addEventListener("mouseover", function( event ) {   
+  console.log(event.target.style)
+  // highlight the mouseover target
+  event.target.style.backgroundColor = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.backgroundColor = "lightblue";
+  }, 500);
+}, false);
+
